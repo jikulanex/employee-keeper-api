382b3c3aa9ee9f4ea037b3b18c8db1b2397315e3
Siegfred Balona <siegfred.balona@lanexus.com>
2021-06-04 15:17:24 +0800

    Refactor server entry file

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+// Load custom error handler.
+const { errorHandler } = require("./middleware/error");
+
+// Load environment variables.
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to the database.
+connectDB();
 
 // Import `employees` route.
 const employees = require("./routes/employees");
 // Import `skills` route.
 const skills = require("./routes/skills");
-
-// Load environment variables.
-dotenv.config({ path: "./config/config.env" });
 
 // Initialize express application.
 const app = express();
@@ -27,11 +35,23 @@ app.use("/api/v1/employees", employees);
 // Mount the `skills` router.
 app.use("/api/v1/skills", skills);
 
+// Make use of the custom error handler.
+app.use(errorHandler);
+
 // Initialize server port.
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+// Run the server.
+const server = app.listen(port, () => {
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${port}.`
+    `Server running in ${process.env.NODE_ENV} mode on port ${port}.`.white.bold
   );
+});
+
+// Handle unhandled promise rejections.
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`.red);
+
+  // Close the server and exit the process.
+  server.close(() => process.exit(1));
 });
